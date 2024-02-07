ad7e4899aa37c23a7a6dabbdfc60fd5296ee0f65
David Vazquez <dave_u@outlook.com>
2024-02-06 20:02:28 -0600

    a ver

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,33 @@
+import styled from "styled-components";
 
+const HomeContainer = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    font-size: 4.0rem;
+    font-weight: 400;
+    text-align: center;
+    letter-spacing: 2px;
+`;
+const Subtitle = styled.p`
+    font-size: 3.0rem;
+    font-weight: 300;
+    text-align: center;
+    letter-spacing: 1px;
+`;
 
 const Home = () => {
     return (
-        <>
-            <h1>Bienvendio a oultra</h1>
-            <p>Soluciones en Software e Instalaciones</p>
-        </>
+        <HomeContainer>
+            <Title>Bienvendio a oultra</Title>
+            <Subtitle>Soluciones en Software e Instalaciones</Subtitle>
+        </HomeContainer>
     )
 }
 
