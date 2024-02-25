7886189997b54fa861b1e4f361e3847a15487948
David Vazquez <dave_u@outlook.com>
2024-02-25 11:23:48 -0600

    speed insights arreglado

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import Home from "./pages/Home/Home"
 import Navbar from "./components/Nav"
